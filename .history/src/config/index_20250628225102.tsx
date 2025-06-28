85645f6572c3c328c944ce45cbe252579da61dc1
ruddro420 <aliruddro@gmail.com>
2025-06-29 02:05:02 +0600

    working

---
+++ .history/src/config/index_20250628225102.tsx
@@ -0,0 +1,18 @@
+const MAIN_URL = 'https://george-fx.github.io/nuton_api/';
+const UPDATE_URL = 'https://ruddro420.github.io/inspired-it-app-api';
+
+export const GET_COUPONS = `${MAIN_URL}api/coupons.json`;
+export const GET_COURSES = `${MAIN_URL}api/courses.json`;
+export const GET_BANNERS = `${MAIN_URL}api/banners.json`;
+export const GET_CATEGORIES = `${MAIN_URL}api/categories.json`;
+export const GET_UPDATE_CATEGORIES = `${UPDATE_URL}/categories.json`;
+export const GET_UPDATE_BANNERS = `${UPDATE_URL}/banners.json`;
+
+export const URLS = {
+  GET_BANNERS,
+  GET_COURSES,
+  GET_COUPONS,
+  GET_CATEGORIES,
+  GET_UPDATE_CATEGORIES,
+  GET_UPDATE_BANNERS
+};
